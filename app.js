48b22d242abc3422b68496c9c4e509311b9ced23
Xintri <mimosa.kuko@gmail.com>
2025-03-08 12:32:39 -0600

    a

--- app.js
+++ app.js
@@ -422,8 +422,9 @@ app.get("/eliminarExperimento/:id", (req, res) => {
 });
 
 
-
-
+app.get("/", (req, res) => {
+    res.redirect("/obtenerAngeles");  // Aquí rediriges a la ruta que ya está definida
+});
 
 // Servidor corriendo
 app.listen(3000, () => {
