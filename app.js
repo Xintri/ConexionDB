20a22d5f3365b342e0867e607f6d5e5d24199187
Xintri_ <mimosa.kuko@gmail.com>
2025-03-08 21:39:09 -0600

    redirect index sendFile

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile('/opt/render/project/src/public/index.html');
+    res.sendFile('./public/index.html');
 });
 
 
